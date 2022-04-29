e3c659b1a80bfb78ed66f44d18fecd61af3dbe8e
Kewl <ac8736@nyu.edu>
2022-04-29 01:04:34 -0400

    diagloue

---
+++ Necroid Dread/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences = new Queue<string>();
+    public TextMeshProUGUI dialogueText;
+    public Animator animator;
+    public void StartDialogue(Dialogue dialogue)
+    {
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    void EndDialogue()
+    {
+        animator.SetTrigger("Close");
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+}
---
+++ Necroid Dread/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public Animator animator;
+    void Start() {
+        StartCoroutine(delay());
+    }
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            FindObjectOfType<DialogueManager>().DisplayNextSentence();
+        }
+    }
+    public void TriggerDialogue() {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    IEnumerator delay() {
+        yield return new WaitForSeconds(0.5f);
+        animator.SetTrigger("Open");
+        TriggerDialogue();
+    }
+}
