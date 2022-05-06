fe62b92a00bfc3c029dd0f214593ff3761584574
SunJames568 <sunjames568@gmail.com>
2022-05-06 01:31:22 -0400

    Armor pickup sound attempt

--- Necroid Dread/Assets/Scripts/Player/Armor Pickup/ArmorPickup.cs
+++ Necroid Dread/Assets/Scripts/Player/Armor Pickup/ArmorPickup.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class ArmorPickup : MonoBehaviour
 {
+    public AudioClip PickupSound;
+    AudioSource _audio;
+
+    void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerMovement>().animator.SetBool("UpgradedArmor", true);
             other.GetComponent<PlayerMovement>().upgrade = true;
-            Destroy(gameObject);
+            _audio.PlayOneShot(PickupSound);
+            Destroy(this.gameObject);
         }
     }
 }
