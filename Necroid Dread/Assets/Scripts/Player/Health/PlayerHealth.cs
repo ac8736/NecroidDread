41d5989cc9d2eadd09e498e30b9ce5e1b545a64b
Kewl <87680132+ac8736@users.noreply.github.com>
2022-05-05 23:31:43 -0400

    respawn

--- Necroid Dread/Assets/Scripts/Player/Health/PlayerHealth.cs
+++ Necroid Dread/Assets/Scripts/Player/Health/PlayerHealth.cs
@@ -12,6 +12,7 @@ public class PlayerHealth : MonoBehaviour
     public Healthbar shieldbar;
     public GameObject shieldUI;
     bool shieldActive = false;
+    public Transform spawnPoint;
     Animator animator;
     void Start()
     {
@@ -19,37 +20,62 @@ public class PlayerHealth : MonoBehaviour
         currentHealth = maxHealth;
         healthbar.SetMaxHealth(maxHealth);
     }
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.CompareTag("MonsterBullet")) {
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+            healthbar.SetMaxHealth(maxHealth);
+            transform.position = spawnPoint.position;
+            if (shieldActive)
+            {
+                currentShield = maxShield;
+                shieldbar.SetMaxHealth(maxShield);
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("MonsterBullet"))
+        {
             TakeDamage(5);
         }
     }
-    void TakeDamage(float damage) {
-        if (shieldActive && currentShield > 0) {
+    void TakeDamage(float damage)
+    {
+        if (shieldActive && currentShield > 0)
+        {
             animator.SetTrigger("shieldDmg");
             currentShield -= damage;
             shieldbar.SetHealth(currentShield);
             StopAllCoroutines();
             StartCoroutine(initializeShieldRegeneration());
-        } else {
+        }
+        else
+        {
             animator.SetTrigger("healthDmg");
             currentHealth -= damage;
             healthbar.SetHealth(currentHealth);
         }
     }
-    public void obtainShield() {
+    public void obtainShield()
+    {
         shieldActive = true;
         currentShield = maxShield;
         shieldbar.SetMaxHealth(maxShield);
         shieldUI.SetActive(true);
     }
-    IEnumerator initializeShieldRegeneration() {
+    IEnumerator initializeShieldRegeneration()
+    {
         yield return new WaitForSeconds(5);
         StartCoroutine(regenerateShield());
     }
-    IEnumerator regenerateShield() {
-        while (currentShield < maxShield) {
-            if (currentShield + 0.1f <= maxShield) {
+    IEnumerator regenerateShield()
+    {
+        while (currentShield < maxShield)
+        {
+            if (currentShield + 0.1f <= maxShield)
+            {
                 currentShield += 0.1f;
                 shieldbar.SetHealth(currentShield);
             }
