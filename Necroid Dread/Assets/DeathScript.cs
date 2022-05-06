e1a4ab30ad1e6e090fc8ec69e7344947b047400a
Kewl <87680132+ac8736@users.noreply.github.com>
2022-05-05 22:07:23 -0400

    push

--- Necroid Dread/Assets/DeathScript.cs
+++ Necroid Dread/Assets/DeathScript.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Door : MonoBehaviour
+public class DeathScript : MonoBehaviour
 {
-    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(sceneName);
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(100);
         }
     }
 }
